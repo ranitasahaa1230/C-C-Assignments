388887797588f12701d71230a44dd9e00687949e
Ranita Saha <saharanitaa1230@gmail.com>
2022-07-31 11:07:18 +0530

    Update assignment-6.c

--- assignment-6.c
+++ assignment-6.c
@@ -1,16 +1,26 @@
 // 1. Write a program to calculate sum of first N natural numbers
 
-#include<stdio.h>
-int main(){
-    printf("\\\\");
-    return 0;
-}
+// #include<stdio.h>
+// int main(){
+//     int i,num,sum=0;
+//     printf("Enter the number: ");
+//     scanf("%d",&num);
+//     for(i=1;i<=num;i++)
+//     sum+=i;
+//     printf("The sum of first %d natural numbers is %d",num,sum);
+//     return 0;
+// }
 
 // 2. Write a program to calculate sum of first N even natural numbers
 
 // #include<stdio.h>
 // int main(){
-//     printf("\\\\");
+//     int i,num,sum=0;
+//     printf("Enter the number: ");
+//     scanf("%d",&num);
+//     for(i=1;i<=num;i++)
+//     sum+=2*i;
+//     printf("The sum of first %d even natural numbers is %d",num,sum);
 //     return 0;
 // }
 
@@ -18,7 +28,12 @@ int main(){
 
 // #include<stdio.h>
 // int main(){
-//     printf("\\\\");
+//     int i,num,sum=0;
+//     printf("Enter the number: ");
+//     scanf("%d",&num);
+//     for(i=1;i<=num;i++)
+//     sum+=2*i-1;
+//     printf("The sum of first %d odd natural numbers is %d",num,sum);
 //     return 0;
 // }
 
@@ -26,7 +41,12 @@ int main(){
 
 // #include<stdio.h>
 // int main(){
-//     printf("\\\\");
+//     int i,num,sum=0;
+//     printf("Enter the number: ");
+//     scanf("%d",&num);
+//     for(i=1;i<=num;i++)
+//     sum+=i*i;
+//     printf("The sum of squares of first %d natural numbers is %d",num,sum);
 //     return 0;
 // }
 
@@ -34,7 +54,12 @@ int main(){
 
 // #include<stdio.h>
 // int main(){
-//     printf("\\\\");
+//     int i,num,sum=0;
+//     printf("Enter the number: ");
+//     scanf("%d",&num);
+//     for(i=1;i<=num;i++)
+//     sum+=i*i*i;
+//     printf("The sum of cubes of first %d natural numbers is %d",num,sum);
 //     return 0;
 // }
 
@@ -50,7 +75,15 @@ int main(){
 
 // #include<stdio.h>
 // int main(){
-//     printf("\\\\");
+//     int num,count=0,k;
+//     printf("Enter the number: ");
+//     scanf("%d",&num);
+//     k=num;
+//     while (num!=0){
+//         num=num/10;
+//         count++;
+//     }
+//     printf("The count of digits of %d is %d",k,count);
 //     return 0;
 // }
 
@@ -58,9 +91,16 @@ int main(){
 
 // #include<stdio.h>
 // int main(){
-//     printf("\\\\");
+//     int num;
+//     printf("Enter the number: ");
+//     scanf("%d",&num);
+//     if(num%1==0 && num%num==0)
+//         printf("%d is a prime number",num);
+//     else
+//         printf("%d is not a prime number",num);
 //     return 0;
 // }
+
 
 // 9. Write a program to calculate LCM of two numbers
 
@@ -72,8 +112,13 @@ int main(){
 
 // 10. Write a program to reverse a given number
 
-// #include<stdio.h>
-// int main(){
-//     printf("\\\\");
-//     return 0;
-// }
+#include<stdio.h>
+int main(){
+    int i,num,reverse;
+    printf("Enter the number: ");
+    scanf("%d",&num);
+    for(i=1;i<=num;i++)
+    reverse+=i*i*i;
+    printf("The reverse of a given number %d is %d",num,reverse);
+    return 0;
+}
