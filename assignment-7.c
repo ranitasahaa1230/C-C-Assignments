dab4c1f1c9b82d0f02b0f00a40cc9e8d4092f678
Ranita Saha <saharanitaa1230@gmail.com>
2022-08-22 10:17:45 +0530

    Update assignment-7.c

--- assignment-7.c
+++ assignment-7.c
@@ -1,26 +1,45 @@
 // 1. Write a program to find the Nth term of the Fibonnaci series.
 
-#include<stdio.h>
-int main(){
-    printf("\\\\");
-    return 0;
-}
+// #include<stdio.h>
+// int main(){
+//     int firstTerm=0,secondTerm=1,num,i;
+//     int nextTerm=firstTerm + secondTerm;
+//     printf("Enter the number: ");
+//     scanf("%d",&num);
+//     printf("Fibonnaci Series : %d, %d ",firstTerm, secondTerm);
+//     
+//     return 0;
+// }
 
 // 2. Write a program to print first N terms of Fibonacci series
 
-// #include<stdio.h>
-// int main(){
-//     printf("\\\\");
-//     return 0;
-// }
+#include<stdio.h>
+int main(){
+    int n,firstTerm,secondTerm,nextTerm;
+    firstTerm=0;
+    secondTerm=1;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    printf("Fibonacci series: %d %d ",firstTerm,secondTerm);
+    for(int i=2;i<n;i++){
+        nextTerm=firstTerm+secondTerm;
+        printf("%d ",nextTerm);
+        firstTerm=secondTerm;
+        secondTerm=nextTerm;
+    }
+    return 0;
+}
 
 // 3. Write a program to check whether a given number is there in the Fibonacci series or not.
 
-// #include<stdio.h>
-// int main(){
-//     printf("\\\\");
-//     return 0;
-// }
+#include<stdio.h>
+int main(){
+    int i,n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+    if((5*n*n+4)*(5*n*n+4) || (5*n*n-4)(5*n*n-4))
+    return 0;
+}
 
 // 4. Write a program to calculate HCF of two numbers
 
