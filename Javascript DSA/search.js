ce329bda9c99bcfd1de4b39109009c51ba39bf15
Ranita Saha <saharanitaa1230@gmail.com>
2024-01-08 18:00:45 +0530

    feat:add sort and search

--- Javascript DSA/search.js
+++ Javascript DSA/search.js
@@ -58,7 +58,42 @@ const findKthPositive = (arr, k) => {
         count++;
     }
     return k+count;
+    //2=>5+0=>3=>6=>4=>7;7=>8;11=>9
+    //count=>0=>1=>2;3;
 };
 
-console.log(findKthPositive([2,3,4,7,11], 5));
-console.log(findKthPositive([1,2,3,4], 2));
+// console.log(findKthPositive([2,3,4,7,11], 5));
+// console.log(findKthPositive([1,2,3,4], 2));
+
+var maximumCount = function(nums) {
+    return Math.max(upperBound(nums),lowerBound(nums));
+};
+
+function upperBound(nums){
+    let low=0;
+    let high=nums.length-1;
+
+    while(low<high){
+        let mid=Math.ceil((low+high)/2);
+
+        if(nums[mid]<0) low=mid;
+        else high=mid-1;
+    }
+    return nums[0] >= 0 ? 0 : low+1;
+}
+
+function lowerBound(nums){
+    let low=0;
+    let high=nums.length-1;
+
+    while(low<high){
+        let mid=Math.floor((low+high)/2);
+
+        if(nums[mid]>0) high=mid;
+        else low=mid+1;
+    }
+    return nums[nums.length-1] <= 0 ? 0 : nums.length-low;
+}
+
+console.log(maximumCount( [-2,-1,-1,1,2,3]));
+console.log(maximumCount([-3,-2,-1,0,0,1,2]));
