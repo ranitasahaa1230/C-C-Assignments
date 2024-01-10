b791d19f70bb0f38bd26fde0fa48720f84eb394d
Ranita Saha <saharanitaa1230@gmail.com>
2024-01-10 21:36:06 +0530

    feat:folder added

--- javascript/mergenquicksort.js
+++ javascript/mergenquicksort.js
@@ -27,7 +27,7 @@ function merge(left,right){//0(n)=>compring every single element
 //TC:0(nlogn)
 //SC:0(N)=>created one arr
 const arr=[8,3,4,5,7,6,1,2]
-console.log(mergeSort(arr))
+// console.log(mergeSort(arr))
 
 const mergeInterval=(intervals)=>{
     let startIndex=0;
@@ -37,14 +37,55 @@ const mergeInterval=(intervals)=>{
 
     let mergeInt=[];
     let mergeStart=intervals[0][startIndex];
-    let mergeEnd=intervals[0][endIndex];
+    let mergeEnd=intervals[0][endIndex];//[1,3]
 
     for(let i=1;i<intervals.length;i++){
-        let 
+        const subsequentIntervals=intervals[i] 
+// console.log(subsequentIntervals[startIndex])
+        if(subsequentIntervals[startIndex]<=mergeEnd){
+            mergeEnd=Math.max(mergeEnd,subsequentIntervals[endIndex])
+        }else{
+            mergeInt.push([mergeStart,mergeEnd]);
+            console.log(mergeInt)
+
+            mergeStart=subsequentIntervals[startIndex];
+            mergeEnd=subsequentIntervals[endIndex]
+        }
     }
+
+    mergeInt.push([mergeStart,mergeEnd]);
+    return mergeInt
 
 }
 
 const intervals=[[1,3],[2,6],[8,10],[15,18]]
-console.log(mergeInterval(intervals))
+// console.log(mergeInterval(intervals))
 
+//quicksort
+//daytodywork, fast and saves memory
+
+const quicksort=(arr)=>{
+    if(arr.length<=1) return arr;
+
+    let pivot=arr[0];//DIVIND ARR INTO 2 PARTS
+    let leftArr=[];
+    let rightArr=[];
+
+    for(let i=1;i<arr.length;i++){//O(N)
+        if(arr[i]<pivot){
+            leftArr.push(arr[i]);
+        }else{
+            rightArr.push(arr[i]);
+        }
+    }
+
+    return [...quicksort(leftArr),pivot,...quicksort(rightArr)];
+}
+
+//TC: BC=>O(NLOGN)
+//AC:0(NLOGN)
+//WC:0(N^2)
+//SC: AC=>0(LOGN)=>since using recursion so uses an auxillary space
+//WC=>0(N)
+
+console.log(quicksort(arr))
