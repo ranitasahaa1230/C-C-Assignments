4a71b38250ec7707dbcda0840686ba1dc12e125b
Ranita Saha <saharanitaa1230@gmail.com>
2024-01-16 06:26:31 +0530

    feat:add doubly ll done

--- Javascript DSA/doublyLinkedList.js
+++ Javascript DSA/doublyLinkedList.js
@@ -85,10 +85,78 @@ class DoublyLinkedList{
     }
 
     addAt(index,data){
+        if(index<0 || index> this.size()){
+            console.error("Invalid index");
+            return;
+        }
 
+        const newNode=new Node(data);
+        if(index===0){
+            newNode.next=this.head;
+
+            if(this.head){
+            this.head.prev=newNode;
+            }
+            this.head=newNode;
+        }
+
+        let currentNode=this.head;
+        for(let i=0;i<index-1;i++){
+            currentNode=currentNode.next;
+        }
+
+        newNode.prev=currentNode.prev;
+        newNode.next=currentNode;
+        
+        if(currentNode.next){
+            currentNode.next.prev=newNode;
+        }
+        currentNode.next=newNode;
     }
 
     removeAt(index,data){
+         if(index<0 || index> this.size()){
+            console.error("Invalid index");
+            return;
+        }
+
+        if(index===0){
+            this.head=this.head.next;
+
+            if(this.head){
+                this.head.prev=null;
+            }
+        }
+
+        let currentNode=this.head;
+        for(let i=0;i<index-1;i++){
+            currentNode=currentNode.next;
+        }
+
+        if(currentNode.next){
+            currentNode.next=currentNode.next.next;
+
+            if(currentNode.next){
+                currentNode.next.prev=currentNode;
+            }
+        }
         
     }
 }
+
+const DLL=new DoublyLinkedList();
+DLL.addFirst(5);
+DLL.addFirst(3);
+DLL.addFirst(8);
+DLL.addFirst(89);
+DLL.addLast(6);
+
+DLL.removeTop();
+
+// DLL.addAt(2, 10);
+
+DLL.removeLast();
+DLL.removeAt(2);
+
+DLL.printDoublyLinkedList();
+console.log("size", DLL.size())
