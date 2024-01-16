ba0e5ff430358a479a8e0bd41b01ff8933676eb0
Ranita Saha <saharanitaa1230@gmail.com>
2024-01-16 05:42:20 +0530

    feat:add doubly ll

--- Javascript DSA/doublyLinkedList.js
+++ Javascript DSA/doublyLinkedList.js
@@ -1,1 +1,20 @@
 //Doubly Linked List
+class Node{
+    constructor(data){
+        this.data=data;
+        this.prev=null;
+        this.next=null;
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head=null;
+    }
+
+    addFirst(data){
+        const newNode=new Node(data);
+
+        
+    }
+}
