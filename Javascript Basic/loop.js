e069c98a4fd9ffcf41c3a62f8e95cba0e1374c86
Ranita Saha <saharanitaa1230@gmail.com>
2024-01-24 00:41:44 +0530

    feat:add arrays reduce

--- Javascript Basic/loop.js
+++ Javascript Basic/loop.js
@@ -36,85 +36,123 @@ const countDogits = (n) => {
 // console.log(countDogits(34252));
 // console.log(countDogits(-34252));
 
-const reverseNumber=(n)=>{
-    let reverseNum=0,copyNum=n;
-   while(copyNum>0){//TLE
-    let rem=copyNum%10;
-    reverseNum=reverseNum*10+rem;
-    copyNum=Math.floor(copyNum/10);
-   }
-   return reverseNum;
-}
+const reverseNumber = (n) => {
+  let reverseNum = 0,
+    copyNum = n;
+  while (copyNum > 0) {
+    //TLE
+    let rem = copyNum % 10;
+    reverseNum = reverseNum * 10 + rem;
+    copyNum = Math.floor(copyNum / 10);
+  }
+  return reverseNum;
+};
 
 const isPalindrome = (n) => {
-    let r=reverseNumber(n);
-   return r===n;
+  let r = reverseNumber(n);
+  return r === n;
 };
 //   console.log(isPalindrome(452));
 //   console.log(isPalindrome(121));
 
-var fib = function(n) {
-    if(n<2) return n;
-    let prev=0,curr=1,next;
-    for(let i=2;i<=n;i++){
-        next=prev+curr;
-        prev=curr;
-        curr=next;
-    }
-    return next;
-    
+var fib = function (n) {
+  if (n < 2) return n;
+  let prev = 0,
+    curr = 1,
+    next;
+  for (let i = 2; i <= n; i++) {
+    next = prev + curr;
+    prev = curr;
+    curr = next;
+  }
+  return next;
 };
 //   console.log(fib(3));
 
-var missingNumber = function(nums) {
-    let sum=0
-    for(let i=0;i<nums.length;i++){
-        sum+=nums[i];
-    }
-    return nums.length*(nums.length+1)/2-sum;
+var missingNumber = function (nums) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
+  return (nums.length * (nums.length + 1)) / 2 - sum;
 };
 //   console.log(missingNumber([9,6,4,2,3,5,7,0,1]));
 
 const employees = [
-    { name: 'John', age: 32 },
-    { name: 'Sarah', age: 28 },
-    { name: 'Michael', age: 40 },
+  { name: "John", age: 32 },
+  { name: "Sarah", age: 28 },
+  { name: "Michael", age: 40 },
 ];
-const employeesName=employees.map((i)=>i.name);
+const employeesName = employees.map((i) => i.name);
 // console.log(employeesName)
 
 const products = [
-    { name: 'iPhone', price: 999, inStock: true },
-    { name: 'Samsung Galaxy', price: 899, inStock: false },
-    { name: 'Google Pixel', price: 799, inStock: true },
+  { name: "iPhone", price: 999, inStock: true },
+  { name: "Samsung Galaxy", price: 899, inStock: false },
+  { name: "Google Pixel", price: 799, inStock: true },
 ];
-const inStockProd=products.filter((i)=>i.inStock);
+const inStockProd = products.filter((i) => i.inStock);
 // console.log(inStockProd)
 
 const orders = [
-    { product: 'iPhone', price: 999, quantity: 2 },
-    { product: 'Samsung Galaxy', price: 899, quantity: 1 },
-    { product: 'Google Pixel', price: 799, quantity: 3 },
+  { product: "iPhone", price: 999, quantity: 2 },
+  { product: "Samsung Galaxy", price: 899, quantity: 1 },
+  { product: "Google Pixel", price: 799, quantity: 3 },
 ];
-const sum=orders.reduce((acc,cval)=>acc+cval.price*cval.quantity,0);
+const sum = orders.reduce((acc, cval) => acc + cval.price * cval.quantity, 0);
 // console.log(sum)
 
-const words = ['apple', 'banana', 'cherry', 'dragonfruit', 'elderberry'];
-const longWord=words.reduce((longestWord,word)=>word.length> longestWord.length ? word : longestWord ,"");
+const words = ["apple", "banana", "cherry", "dragonfruit", "elderberry"];
+const longWord = words.reduce(
+  (longestWord, word) =>
+    word.length > longestWord.length ? word : longestWord,
+  ""
+);
 // console.log(longWord.length);
 // console.log(longWord);
 
 const numbers = [5, 2, 8, 4, 3];
-const largestNumFact=numbers.reduce((great,num)=>num>great ? num : great,0);
-console.log(largestNumFact)
+// const largestNumFact=numbers.reduce((great,num)=>num>great ? num : great,0);
+// let fact=1;
+// for(i=1;i<=largestNumFact;i++){
+//     fact*=i;
+// }
+// console.log(fact);
+const currFact = numbers.reduce((great, num) => {
+  let currentFactorial = Array.from({ length: num })
+    .map((_, i) => i + 1)
+    .reduce((fact, val) => fact * val, 1);
+
+  return currentFactorial > great ? currentFactorial : great;
+});
+console.log(currFact);
 
 const students = [
-    { name: 'John', score: 85 },
-    { name: 'Sarah', score: 92 },
-    { name: 'Michael', score: 88 },
-    { name: 'Emma', score: 95 },
-    { name: 'Daniel', score: 90 },
+  { name: "John", score: 85 },
+  { name: "Sarah", score: 92 },
+  { name: "Michael", score: 88 },
+  { name: "Emma", score: 95 },
+  { name: "Daniel", score: 90 },
 ];
-const filterStu=students.filter((s)=>s.score>90).reduce((acc,cval)=>(acc+cval.score)/2,0);
-console.log(filterStu)
+const filterStu = students
+  .filter((s) => s.score > 90)
+  .reduce((acc, cval, i, arr) => acc + cval.score / arr.length, 0);
+// console.log(filterStu);
 
+const books = [
+  { title: "Book 1", year: 1998 },
+  { title: "Book 2", year: 2003 },
+  { title: "Book 3", year: 1995 },
+  { title: "Book 4", year: 2001 },
+];
+const filterBooks = books.filter((s) => s.year < 2000).map((acc) => acc.title);
+// console.log(filterBooks);
+
+const strings = ["hello world", "i am openai", "welcome to javascript"];
+const charAtCapital = strings.map((s) =>
+  s
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
+    .join(" ")
+);
+console.log(charAtCapital);
