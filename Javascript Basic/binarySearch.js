6871c12384bfb85c15b0df7eaea859947ea3f350
Ranita Saha <saharanitaa1230@gmail.com>
2024-01-25 17:17:57 +0530

    feat: add binary search

--- Javascript Basic/binarySearch.js
+++ Javascript Basic/binarySearch.js
@@ -5,4 +5,20 @@ const linearSearch=(arr,target)=>{
     }
     return -1;
 }
-console.log(linearSearch([1,2,6,9,0,-5],8))
+// console.log(linearSearch([1,2,6,9,0,-5],8))
+
+const binarySearch=(arr,target)=>{
+    let start=0,end=arr.length-1;
+    while(start<=end){
+        let mid=Math.floor((start+end)/2);
+
+        if(arr[mid]===target) return mid;
+        else if(arr[mid]<target){
+            start=mid+1;
+        }else{
+            end=mid-1;
+        }
+    }
+    return -1;
+}
+console.log(binarySearch([1,2,6,9,0,-5],8))
